c9f323b79e627ff87f05b803137765eea720dcc1
Marlena Baker <baker.marlena@gmail.com>
2017-07-11 14:45:11 -0600

    small fixes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const cors = require('cors')
 
 var app = express();
 
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use("/", router);
 
